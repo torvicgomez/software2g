bc6d6e32d844eafcc4bf000cf1ea06de8a621666
Torsha.Proyectos <torsha.proyectos@gmail.com>
2015-02-02 03:38:42 +0000

    agenda de citas medicas

--- Software2G/WebContent/js/constantes.js
+++ Software2G/WebContent/js/constantes.js
@@ -1,7 +1,7 @@
 var paca_slotduration = '00:30:00';
 var paca_scrolltime = '08:00:00';
-var paca_mitime = '04:00:00';
-var paca_maxtime = '22:00:00';
+var paca_mitime = '08:00:00';
+var paca_maxtime = '18:00:00';
 var paca_sloteventover = false;
 var paca_alldayslot = true;
 var paca_alldaytext = 'Todo el Dia';
